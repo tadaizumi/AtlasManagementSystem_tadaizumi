b544ee008dfb8ea7b60e0ce6815458ff3c804b8e
tadaizumi123 <tadaizumi123@gmail.com>
2026-07-31 21:27:03 +0900

    環境構築2-2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Register your seeders here
-        $this->call(SubjectsTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            SubjectsTableSeeder::class,
+        ]);
     }
 }
